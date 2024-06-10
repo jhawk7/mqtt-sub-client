b8300eed7623617af571f6bf72fad631e3902b13
jhawk7 <jhawk@ser>
2024-06-10 19:56:03 -0400

    setting up notifier module

--- mqtt-sub/internal/notify/notify.go
+++ mqtt-sub/internal/notify/notify.go
@@ -1,11 +1,37 @@
 package notify
 
+import (
+	"fmt"
+	"mqtt-sub/internal/common"
+	"net/smtp"
+	"strings"
+)
+
 type INotifier interface {
+	Notify(string) error
 }
 
-type notifier struct {
+type Notifier struct {
+	email string
+	pass  string
+	host  string
+	to    []string
 }
 
-func InitNotifier() INotifier {
+func InitNotifier(c *common.Config) INotifier {
+	return &Notifier{
+		email: c.ClientFrom,
+		pass:  c.SMTPPass,
+		host:  c.SMTPServer,
+		to:    c.ClientsTo,
+	}
+}
 
+func (n *Notifier) Notify(alertmsg string) error {
+	auth := smtp.PlainAuth("", n.email, n.pass, n.host)
+	if sErr := smtp.SendMail(n.host, auth, n.email, n.to, []byte(alertmsg)); sErr != nil {
+		return fmt.Errorf("failed to send msg via smtp; %v", sErr)
+	}
+	common.LogInfo(fmt.Sprintf("successfully notified recipients %v", strings.Join(n.to, ",")))
+	return nil
 }
